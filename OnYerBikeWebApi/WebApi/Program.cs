e9d45d8561d44239048341d49a4846f6e368804a
David Johnson <David.Johnson@europa-worldwide.com>
2023-01-30 17:48:32 +0000

    Tidy Program.cs up

--- OnYerBikeWebApi/WebApi/Program.cs
+++ OnYerBikeWebApi/WebApi/Program.cs
@@ -3,11 +3,8 @@ using WebApi;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//Resolve any dependency injected instances
 builder.Services.AddDiDependencies(builder);
 builder.Services.AddAuthentication();
 builder.Services.ConfigureIdentity();
